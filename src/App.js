efc10035fc2a69c904cee93cc89bcd94f4771b0b
Maryam Ebrahim <mxebrahim>
2018-08-25 15:16:15 +0100

    replace  lable for to htmlFor

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ class App extends React.Component {
         <div className="app-header"><h2 className="header-title"> Putney Neighbourhood's Cafes </h2></div>
         <div className="off-canvas">
           <input id="hamburger" type='checkbox' className="hamburger-checkbox" />
-          <label for="hamburger" className="hamburger-label" role="button" aria-labelledby="menu"><FontAwesomeIcon icon="bars"/></label>
+          <label className="hamburger-label"  htmlFor="hamburger" role="button" aria-labelledby="menu"><FontAwesomeIcon icon="bars"/></label>
           <nav id="list-toggle" role="list" className="sidebar" onClick={this.toggleList}>
             <div className="list-view">
               <ListView cafes={this.state.cafes} clickedMarker={this.clickedMarker} filterMarker={this.filterMarker}  />
